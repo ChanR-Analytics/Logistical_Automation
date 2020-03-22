6bfae48769ecf9e1036dc77e23586c50bf78483c
TheChanRProject <garpchoo@gmail.com>
2020-03-22 03:16:07 -0700

    Commiting my changes

--- gmail_to_slides_parser.py
+++ gmail_to_slides_parser.py
@@ -1,5 +1,3 @@
-import re
-
 def gmail_to_slides_parser(contact_list: str) -> str:
     contacts_as_list = contact_list.split(",")
     emails = [email[email.index("<")+1:-1] for email in contacts_as_list]
